be62082cd2a36e3ec80c6a36fd7350f8d991daba
Marcus Rohrmoser <work@mro.name>
2020-03-29 10:58:58 +0200

    refactor common code

--- lib/lines.ml
+++ lib/lines.ml
@@ -1,29 +1,26 @@
 
 open Bigarray
 
-(* walk back until we hit the begin of the line *)
-let rec back arr idx =
-  let i = idx - 1 in
-  if i < 0
-  then 0
+let rec step arr dx look stop idx =
+  if idx = stop
+  then stop
   else 
-    if '\n' = arr.{i}
-    then i + 1
-    else back arr i
+    if '\n' = arr.{idx + look}
+    then idx
+    else step arr dx look stop (idx + dx)
+
+(* walk back until we hit the begin of the line *)
+let back arr idx =
+  step arr (-1) (-1) 0 idx
 
 (* walk forward until we hit the end of the line *)
-let rec forw arr max idx =
-  if idx = max 
-  then max - 1
-  else
-    if '\n' = arr.{idx}
-    then idx - 1
-    else forw arr max (idx + 1)
+let forw arr len idx =
+  step arr 1 0 len idx
 
-let line arr max idx =
-  let off = back arr idx
-  and en  = forw arr max idx in
-  let len = en - off + 1 in
-  Array1.sub arr off len
+let line arr len idx =
+  let off  = back arr idx
+  and pos  = forw arr len idx in
+  let len' = pos - off in
+  Array1.sub arr off len'
    |> Bigstring.to_string
 
